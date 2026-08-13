926a3144d020ded1e7de6f70561c9418f18f523d
tisebjhb025 <tisebjhb025@student.wethinkcode.co.za>
2026-08-13 17:42:07 +0200

    feat: added method that enables reading of csv file

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
@@ -1,8 +1,31 @@
 package co.wethinkcode.healthsafe;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import io.javalin.Javalin;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class IngestionServiceApp {
+
+    public static void readDataLineByLine(String file){
+        try {
+            FileReader filereader = new FileReader(file);
+
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+
+            while ((nextRecord = csvReader.readNext()) != null){
+                nextRecord[0].strip().toUpperCase();
+
+            }
+        } catch (IOException | CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7030);
@@ -12,5 +35,7 @@ public class IngestionServiceApp {
         // TODO: read and clean src/main/resources/wards-outdated.csv (wards, wings, specialist departments data —
         // trim whitespace, fix casing, normalize dates/booleans) and expose the
         // cleaned records here for the other services to consume.
+
+        readDataLineByLine("src/main/resources/wards-outdated.csv");
     }
 }
