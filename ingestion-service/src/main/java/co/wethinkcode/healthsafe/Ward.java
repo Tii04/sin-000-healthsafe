10707a5fbad203df54ffaaaedaad1d72be86e470
tisebjhb025 <tisebjhb025@student.wethinkcode.co.za>
2026-08-21 05:10:49 +0200

    feat: adding ward object
    I added a ward object as this is cleaner code and easier to clean the csv

---
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/Ward.java
@@ -0,0 +1,55 @@
+package co.wethinkcode.healthsafe;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ward {
+    private String wardId;
+    private String wing;
+    private String department;
+    private int bedsAvailable;
+
+    public static List<String[]> readCsv(String file){
+        List<String[]> readRecords = new ArrayList<>();
+
+        try {
+
+            // Create an object of filereader
+            // class with CSV file as a parameter.
+            FileReader filereader = new FileReader(file);
+
+            // create csvReader object passing
+            // file reader as a parameter
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+
+            // we are going to read data line by line
+            while ((nextRecord = csvReader.readNext()) != null) {
+                for (int i = 0; i < nextRecord.length; i++) {
+                    nextRecord[i] = nextRecord[i].strip();
+                }
+                readRecords.add(nextRecord);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return readRecords;
+    }
+
+    public static List<String[]> cleanWardId(List<String[]> records){
+        for(int i = 0; i < records.size(); i++){
+            String wardId = records.get(i)[0];
+            wardId = wardId.strip().toUpperCase();
+            records.get(i)[0] = wardId;
+        }
+        return records;
+    }
+
+    public static void main(String[] args){
+        cleanWardId(readCsv("src/main/resources/wards-outdated.csv"));
+    }
+}
