745590dc560d09d05a1d958f3ebdc9b9f57aadec
tisebjhb025 <tisebjhb025@student.wethinkcode.co.za>
2026-09-17 19:59:59 +0200

    test: added basic tests for IngestionService

---
+++ ingestion-service/src/test/java/co/wethinkcode/healthsafe/TestIngestionServiceApp.java
@@ -0,0 +1,52 @@
+package co.wethinkcode.healthsafe;
+
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestIngestionServiceApp {
+
+    List<String[]> records = IngestionServiceApp.readCsv("src/main/resources/wards-outdated.csv");
+    List<String[]> cleanedRecords = IngestionServiceApp.cleanAll(records);
+    List<Ward> ward = IngestionServiceApp.createWards(cleanedRecords);
+
+    @Test
+    public void wardIdCapitalised(){
+
+        assertEquals("W-07", ward.get(6).getWardId());
+    }
+
+    @Test
+    public void wingStartCased(){
+
+        assertEquals("East Wing", ward.get(4).getWing());
+    }
+
+    @Test
+    public void departmentStartCased(){
+
+        assertEquals("Cardiology", ward.get(6).getDepartment());
+    }
+
+    @Test
+    public void normalisationDepartmentOfDifferentSpelling(){
+        String misspelledWord = "PAEDIATRICS";
+
+        cleanedRecords.get(16)[2] = misspelledWord;
+        IngestionServiceApp.cleanDepartment(cleanedRecords);
+        assertEquals("Pediatrics", cleanedRecords.get(16)[2]);
+    }
+
+    @Test
+    public void invalidBedAmountReturnsNull(){
+        String availableBeds = "-6";
+
+        records.get(11)[3] = availableBeds;
+
+        IngestionServiceApp.cleanBedsAvailable(records);
+        assertNull(records.get(11)[3]);
+    }
+}
