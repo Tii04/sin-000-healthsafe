afd976afe48d3504b0bb3dfa587cc8aa01622d72
tisebjhb025 <tisebjhb025@student.wethinkcode.co.za>
2026-09-22 11:02:35 +0200

    feat: added endpoints for wards and departments in wardServiceApp

--- ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
@@ -1,13 +1,54 @@
 package co.wethinkcode.healthsafe;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
 
 public class WardServiceApp {
 
-    public static void main(String[] args) {
+    public WardServiceApp() throws IOException{
+    }
+
+    public static List<Ward> getWardsFromIngestionService() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:7030/wards"))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            List<Ward> wards = mapper.readValue(response.body(), new TypeReference<>() {
+            });
+            return wards;
+        } catch (IOException | InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         Javalin app = Javalin.create().start(7031);
 
         app.get("/health", ctx -> ctx.result("OK"));
+        app.get("/wards", ctx -> {
+            List<Ward> wards = getWardsFromIngestionService();
+            ctx.json(wards);
+        });
+        app.get("/departments", ctx -> {
+            List<Ward> wards = getWardsFromIngestionService();
+            List<String> departments = wards.stream().map(Ward::getDepartment).distinct().toList();
+            ctx.json(departments);
+        });
 
         // TODO (Provides lists of wards and departments.)
         // Add domain endpoints for ward-service here.
