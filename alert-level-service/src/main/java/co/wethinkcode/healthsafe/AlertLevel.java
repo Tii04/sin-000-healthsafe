b197b8373814817e6a7177ac990b907529753b92
tisebjhb025 <tisebjhb025@student.wethinkcode.co.za>
2026-09-22 21:06:08 +0200

    feat: creating alertlevel class

---
+++ alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevel.java
@@ -0,0 +1,15 @@
+package co.wethinkcode.healthsafe;
+
+public class AlertLevel {
+    private int level;
+
+    public AlertLevel(){};
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void setLevel(int level){
+        this.level = level;
+    };
+}
